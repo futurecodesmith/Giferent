a521bf774347002c40cd864cf71de459e4164b97
Jordan Bautista <jordanbau@gmail.com>
2017-03-31 14:09:36 -0700

    committing all files

---
+++ build/0.9294a7f1d7327ebad9cb.hot-update.js
@@ -0,0 +1,8 @@
+webpackHotUpdate(0,[
+/* 0 */
+/***/ (function(module, exports) {
+
+module.exports = "<!doctype html>\n\n<head>\n  <title>Giferent</title>\n</head>\n\n<body>\n  <span>Hello</span>\n  <link rel=\"stylesheet\" type=\"text/css\" href=\"styles.css\"/>\n  <script src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js\"></script>\n  <div id=\"root\"></div>\n  <script src=\"bundle.js\" type=\"text/javascript\"></script>\n</body>\n";
+
+/***/ })
+])
